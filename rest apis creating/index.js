3dc27207507fd6015461ac441717d39b8167b1ef
Raza Muhammad <waistband161@gmail.com>
2025-01-31 16:23:09 +0500

    api creating

--- rest apis creating/index.js
+++ rest apis creating/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-
+const users = require('./MOCK_DATA.json')
 PORT = 8000;
+
+app.get('/users', (req, res)=>{
+    return res.json(users)
+})
 
 app.listen(PORT, ()=>{
     console.log(`server started at ${PORT}`);
