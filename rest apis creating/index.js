98813ec39c993a73cf2f96453e18d2df3e07cf1d
Raza Muhammad <waistband161@gmail.com>
2025-02-03 22:35:41 +0500

    postman se post krna

--- rest apis creating/index.js
+++ rest apis creating/index.js
@@ -3,8 +3,29 @@ const app = express()
 const users = require('./MOCK_DATA.json')
 PORT = 8000;
 
-app.get('/users', (req, res)=>{
+
+// Middleware
+
+app.use(express.urlencoded({extended: false}));
+
+// api to get all users
+app.get('/api/users', (req, res)=>{
     return res.json(users)
+})
+
+
+// api to get user with specific id
+app.get('/api/users/:id', (req, res)=>{
+    const id = Number(req.params.id)
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
+})
+
+app.post('/api/users', (req, res)=>{
+    const body = req.body
+    console.log('body', body);
+    return res.json({status: 'pending'})
+    
 })
 
 app.listen(PORT, ()=>{
